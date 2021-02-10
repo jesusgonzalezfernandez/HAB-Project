9f14617c82e8e0168f6fb82aefc5f3dfc656431c
lorenabao <Lore@MacBook-Air-de-Lorena.local>
2021-02-10 19:31:56 +0100

    added cors to index.js

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,7 @@ const isSameUser = require ('./middlewares/isSameUser')
 const isExpert = require ('./middlewares/isExpert')
 const isAuthor = require ('./middlewares/isAuthor')
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
