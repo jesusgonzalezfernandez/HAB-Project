35d87336ddfd1832d92f994e2c72934b189b63dc
pablomartinatanes <pablo.martin.atanes@gmail.com>
2021-02-16 18:42:39 +0100

    avanzando QuestionPortal

--- front/src/Content/QuestionsPortal.js
+++ front/src/Content/QuestionsPortal.js
@@ -1,13 +1,65 @@
 import QuestionsFilter from "./QuestionsFilter";
 import LatestQuestions from "./LatestQuestions";
 import './QuestionsPortal.css'
+import useFetch from "../useFetch";
 
 
 
 // Portal de preguntas
 function QuestionsPortal() {
 
-    const tags = ['tag', 'tag', 'tag', 'tag', 'tag', 'tag', 'tag']
+    // Obtener listado de preguntas
+    let data = useFetch('http://localhost:3001/questions') || []
+    
+    // Contador de tags
+    let counter = {}
+    let counter2 = []
+
+    const mockTags = ['tag', 'tag', 'tag', 'tag', 'tag', 'tag', 'tag']
+
+    console.log(`data para obtener tags: ${data}`);
+
+    // Obtener tags y contador de tags
+    if (data.length >= 1) {
+
+        for (const question of data) {
+
+            const questionTags = question.tags.split(',')
+
+            for(const tag of questionTags){
+
+                let found = counter2.find(tag)
+                
+                console.log(found);
+                // if(!found) {
+                //     counter2.push({tag, count: 1})
+                // } else {
+                //     counter2[found].count ++
+                // }
+
+                console.log(counter2);
+
+            }
+
+        }
+
+        // for (const question of data) {
+
+        //     const questionTags = question.tags.split(',')
+
+        //     for(const tag of questionTags){
+        //         if (!counter[tag]) {
+        //             counter[tag] = 1
+        //         } else {
+        //             counter[tag] ++
+        //         }
+        //     }
+
+        //     console.log(counter);
+
+        // }
+        
+    }
 
     return (
         
@@ -28,28 +80,30 @@ function QuestionsPortal() {
             </aside>
             <LatestQuestions />
             <aside className='right-aside'>
-                <section className="blog">
+                <section className='blog'>
                     <h6>Latest Blog Posts:</h6>
-                    <ul>
+                    <ul className='post-list'>
                        <li>
                            <h6>Lorem ipsum dolor sit amet consectetur.</h6>
-                           <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse exercitationem error placeat.</p>
+                           <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse exercitationem error placeat...</p>
                        </li>
                        <li>
-                           <h6>Lorem ipsum dolor sit amet consectetur.</h6>
-                           <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse exercitationem error placeat.</p>
+                           <h6>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, earum!</h6>
+                           <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse exercitationem error placeat...</p>
                        </li>
                        <li>
-                           <h6>Lorem ipsum dolor sit amet consectetur.</h6>
-                           <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse exercitationem error placeat.</p>
+                           <h6>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt optio odio minus?</h6>
+                           <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse exercitationem error placeat...</p>
                        </li>
                     </ul>
                 </section>
                 <section className="tags">
                     <h6>Common Tags:</h6>
                     <ul>
-                        {tags && tags.map(tag => 
-                            <li>{tag}</li>
+                        {mockTags && mockTags.map(mockTag => 
+                            <a href={'http://localhost:3001/questions?tags=' + mockTag}>
+                                {mockTag}
+                            </a>
                         )}
                     </ul>
                 </section>
