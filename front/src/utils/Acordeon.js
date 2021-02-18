69b90e89deef9aca7d677c8fcdeb7439f73a67f2
pablomartinatanes <pablo.martin.atanes@gmail.com>
2021-02-18 20:16:37 +0100

    acordeon de comentarios a respuestas funcionando

--- front/src/utils/Acordeon.js
+++ front/src/utils/Acordeon.js
@@ -1,19 +1,46 @@
 import { useState } from 'react'
 import './Acordeon.css'
 
-function Acordeon({ children, onChange, parentID }) {
-    const [visible, setVisible] = useState(false)
 
+
+function Acordeon({ children, onChange, parentID, active }) {
+
+    // Controlar la acción del click
     const handleClick = e => {
         e.preventDefault()
-        setVisible(!visible)
-        onChange (parentID)       
+
+        /* 
+        
+            Modifica el valor del estado 'active' a través
+            de la función onChange (que recibe del padre)
+            
+            Si el id del elemento sobre el que se hace click
+            no es igual al activo, setea el elemento
+            actual como el activo.
+
+            Y si el estado activo es el mismo que el actual,
+            lo setea como undefined para que cierre el acordeon.
+            
+        */
+
+        parentID !== active ? onChange(parentID) : onChange(undefined)       
+    
     }
 
     return (
+
         <div className="acordeon">
             <button onClick={handleClick}>Añade un comentario</button>
-            {visible &&
+
+            {/* 
+            
+                Si el id del padre coincide con el id 
+                del elemento seteado como activo, 
+                muestra el contenido
+                
+            */}
+
+            {parentID === active &&
                 <div className="expand">
                     {children}
                 </div>
