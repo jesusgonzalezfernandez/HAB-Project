57f20a004598c4bbe97a120733ee017d37aecbb7
jesusgonzalezfernandez <jesus.glez.fernandez@gmail.com>
2021-02-19 20:52:37 +0100

    funciona la imagen!

--- front/src/Profiles/ExpertProfile.js
+++ front/src/Profiles/ExpertProfile.js
@@ -12,7 +12,7 @@ function ExpertProfile({ data }) {
     <div className='expert-profile-component'>
       <h2>Admin</h2>
       <aside className='expert-profile'>
-      <img src={data.avatar} alt="avatar"/>
+      <img src={`http://localhost:3001/${data.avatar}`} alt="avatar"/>
         <h3>{data.name} {data.surname}</h3>
         {/* <h4>{data.username}</h4> */}
         <button onClick={() => setEditMode(true)}>Editar</button>
