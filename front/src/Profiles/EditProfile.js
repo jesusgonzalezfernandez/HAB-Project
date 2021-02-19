d0b6ea6ea49ca6fad3aff963840d8677e9c01dfa
lorenabao <lorena.bao@outlook.com>
2021-02-19 19:01:19 +0100

    editar peril- incluido cumpleaños y país

--- front/src/Profiles/EditProfile.js
+++ front/src/Profiles/EditProfile.js
@@ -8,32 +8,32 @@ function EditProfile( {reload, data} ) {
     const login = useSelector(state => state.login)
     const { userID } = useParams()
 
-    console.log('Perfil de login -->>     ' + JSON.stringify(login))
-
     const [displayName, setDisplayName] = useState(data.name || '')
     const [displaySurname, setDisplaySurname] = useState(data.surname || '')
+    const [displayBirth, setDisplayBirth] = useState(data.birthData || '')
+    const [displayCountry, setDisplayCountry] = useState(data.country || '')
     const [displayUsername, setDisplayUsername] = useState(data.avatar || '')
 
-    const handleSubmit = e => {
+    const handleSubmit = async e => {
         e.preventDefault()
         const avatar = e.target.avatar.files[0]
         const fd = new FormData()
         fd.append('avatar', avatar)
         fd.append('name', displayName)
         fd.append('surname', displaySurname)
+        fd.append('birthDate', displayBirth)
+        fd.append('country', displayCountry)
         fd.append('username', displayUsername)
         
-        const res = fetch(`http://localhost:3001/users/${userID}`,
+        const res = await fetch(`http://localhost:3001/users/${userID}`,
         {
         
             headers: { 'auth': login.token },
             method: 'PUT',
             body: fd
         })
-            .then(res => res.json)
-            // .then(data => 
-                
-            // )         
+            // .then(res => res.json)
+         
         console.log(res);
 
         reload()
@@ -51,6 +51,8 @@ function EditProfile( {reload, data} ) {
                 <input type="text" placeholder='Username...' value={displayUsername} onChange={e => setDisplayUsername(e.target.value)} />
                 <input type="text" placeholder='Nombre...' value={displayName} onChange={e => setDisplayName(e.target.value)} />
                 <input type="text" placeholder='Apellido...' value={displaySurname} onChange={e => setDisplaySurname(e.target.value)} />
+                <input type="text" placeholder='País...' value={displayCountry} onChange={e => setDisplayCountry(e.target.value)} />
+                <input type="text" placeholder='Fecha de nacimiento...' value={displayBirth} onChange={e => setDisplayBirth(e.target.value)} />
                 <button>Guardar</button>
             </form>
             <button onClick={() => reload()}>Cancelar</button>
