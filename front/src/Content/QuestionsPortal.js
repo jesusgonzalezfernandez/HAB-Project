0ecab56a288f7a17df3cdd77f6f769979fb75c65
pablomartinatanes <pablo.martin.atanes@gmail.com>
2021-02-20 00:30:52 +0100

    avances en questionportal

--- front/src/Content/QuestionsPortal.js
+++ front/src/Content/QuestionsPortal.js
@@ -2,6 +2,7 @@ import QuestionsFilter from "./QuestionsFilter";
 import LatestQuestions from "./LatestQuestions";
 import './QuestionsPortal.css'
 import useFetch from "../useFetch";
+import getTagCount from "../Functions/getTagCount";
 
 
 
@@ -11,36 +12,11 @@ function QuestionsPortal() {
     // Obtener listado de preguntas
     let data = useFetch('http://localhost:3001/questions') || []
     
-    // Contador de tags
-    let counter = []
+    let tagCounter = []
 
-    console.log(`data para obtener tags: ${data}`);
-
-    // Obtener tags y contador de tags
-    if (data.length >= 1) {
-
-        for (const question of data) {
-
-            const questionTags = question.tags.split(',')
-
-            for(const tag of questionTags){
-
-                const index = counter.findIndex(count => count.tag === tag)
-                
-                if(index === -1) {
-                    counter.push({tag, count: 1})
-                } else {
-                    counter[index].count ++
-                }
-            }
-        }
-    }
+    if (data.length >= 1) tagCounter = getTagCount(data)
 
     return (
-        
-        // Falta enviar una función a filterquestions 
-        // para que al lanzar una búsqueda desaparezca
-        // el listado de ultimas preguntas
 
         <div className="questions-portal">
             <aside className='left-aside'>
@@ -53,7 +29,16 @@ function QuestionsPortal() {
                 </div>
                 <QuestionsFilter />
             </aside>
-            <LatestQuestions />
+            <main>
+                <header>
+                    <h1 className=''>Últimas Preguntas</h1>
+                    <div className=''>
+                        <a className='create-question-button' href='http://localhost:3000/create/question'>Haz Tu Consulta</a>
+                    </div>
+                </header>
+                <LatestQuestions />
+
+            </main>
             <aside className='right-aside'>
                 <section className='blog'>
                     <h6>Latest Blog Posts:</h6>
@@ -73,10 +58,13 @@ function QuestionsPortal() {
                     </ul>
                 </section>
                 <section className="tags">
-                    {counter && counter.map(count =>             
-                        <div className='tag-counter'>
-                            <a href={'http://localhost:3001/questions?tags=' + count.tag}>
-                                {count.tag} 
+                    {tagCounter && tagCounter.map( (count, i) =>             
+                        <div key={i} className='tag-counter'>
+                            <a 
+                                // El tag toma diferentes tamaños según el orden en el array
+                                className={i < tagCounter.length / 2 ? ( i < tagCounter.length / 6 ? 'big' : '' ) : 'small'} 
+                                href={'http://localhost:3001/questions?tags=' + count.tag}>
+                                    {count.tag} 
                             </a>
                             <span>x {count.count}</span>
                         </div>
@@ -86,6 +74,5 @@ function QuestionsPortal() {
         </div >
     );
 }
-
 
 export default QuestionsPortal;
