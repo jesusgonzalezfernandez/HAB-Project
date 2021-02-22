0b63abf1e40d7bb89b32c14a23ddc318d28c5946
pablomartinatanes <pablo.martin.atanes@gmail.com>
2021-02-22 21:18:37 +0100

    login error management fixed

--- front/src/User/Login.js
+++ front/src/User/Login.js
@@ -22,7 +22,7 @@ function Login() {
     e.preventDefault()
 
     // Enviar una consulta a la API con el email y la password
-    fetch(
+    const res = await fetch(
       // Dirección:
       `http://localhost:3001/users/login`,
       // Contenido:
@@ -31,43 +31,45 @@ function Login() {
         body: JSON.stringify({ email, password }),
         method: 'POST'
       })
-      .then(res => {
-        if (!res.ok) {
-          console.log('Se ha producido un error');
-          throw res;
-        }
-        console.log('El fetch se ha realizado correctamente');
-        return res.json()
-      })
-      .then(data => {
-        // Enviar objeto action al redux, con el type y los datos obtenidos de la API
-        dispatch({ type: 'login', data })
-      })
-      .catch(e => 
-        // Capturar error
-        e.text().then(e => setError(e))
-      )
+
+    if(!res.ok) {
+
+      console.log('Se ha producido un error');
+      res.text().then(e => setError(e))
+
+    } else {
+
+      console.log('El fetch se ha realizado correctamente');
+      const data = res.json()
+      // Enviar objeto action al redux, con el type y los datos obtenidos de la API
+      dispatch({ type: 'login', data })
+
+    }
         
   }
-
-  if (error) return <div>Correo o contraseña no válidos</div>
-
+  
   if (login) {
     console.log('Logueado con éxito, redirigiendo...')
     console.log(`Username: ${login.username}, Role: ${login.role}, userID: ${login.userID}`);
     return <Redirect to="/" />
   }
-
+  
   return (
     <main className="login main">
+
+      {error && 
+        <div>
+          {error}
+        </div>}
+
       <form className="login form" onSubmit={handleSubmit}>
         <img src={logo} className={'logo'} alt={'logo'} />
         <h2>Inicia sesión:</h2>
         <div>
-          <input className="login input" placeholder="E-mail ..." value={email} onChange={e => setEmail(e.target.value)} />
+          <input className="login input" placeholder="E-mail ..." value={email} onChange={e => setEmail(e.target.value)} required />
         </div>
         <div>
-          <input className="login input" placeholder="Password ..." type="password" value={password} onChange={e => setPassword(e.target.value)} />
+          <input className="login input" placeholder="Password ..." type="password" value={password} onChange={e => setPassword(e.target.value)} required/>
         </div>
         <button className="submit">Iniciar sesión</button>
         <p>
