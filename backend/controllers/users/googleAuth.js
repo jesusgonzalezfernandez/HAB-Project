35828d373ef31fb35b77ca1cfba0e7f42cea24fa
jesusgonzalezfernandez <jesus.glez.fernandez@gmail.com>
2021-02-25 19:36:39 +0100

    googleauth

--- backend/controllers/users/googleAuth.js
+++ backend/controllers/users/googleAuth.js
@@ -1,25 +1,12 @@
-
-// Dependencias
-<<<<<<< HEAD
-// const bcrypt = require('bcrypt');
-// const jwt = require('jsonwebtoken');
-require('dotenv').config()
-
-const {JWT} = require('google-auth-library');
-const keys = require('./jwt.keys.json');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { OAuth2Client } = require('google-auth-library')
+const client = new OAuth2Client(process.env.CLIENT_ID)
+const performQuery = require('../../db/performQuery')
+const session = require('express-session')
 
 
-
-
-// Queries
-// const getUserQuery = require('../../queries/getUserQuery')
-const performQuery = require('../../db/performQuery');
-
-
-
-
-
-const googleUser = async (name, email, picture) => {
+const googleUser = async (given_name, email, picture) => {
 
     const query =
 
@@ -27,7 +14,7 @@ const googleUser = async (name, email, picture) => {
             UPDATE users SET 
 
                 avatar = '${picture}' ,
-                name = '${name}' 
+                name = '${given_name}' 
                 
             WHERE email = '${email}' AND active = true 
         `
@@ -36,53 +23,29 @@ const googleUser = async (name, email, picture) => {
     return result
 
 }
-=======
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const { OAuth2Client } = require('google-auth-library')
-const client = new OAuth2Client(process.env.CLIENT_ID)
-
->>>>>>> b1c95f6f7587fa7067fb014d6670b4319e6dfa3d
 
 const googleAuth = async (req, res) => {
     console.log('*  Google Auth  *')
     try {
 
-<<<<<<< HEAD
-        const { token } = req.body
-
-        const client = new JWT({
-            email: keys.client_email,
-            key: keys.private_key,
-            scopes: ['https://www.googleapis.com/auth/cloud-platform'],
-          });
-
-        const ticket = await client.verifyIdToken({
-            idToken: token,
-            audience: process.env.CLIENT_ID
-        });
-
-        console.log('tiket    ' + ticket)
-
-        const { name, email, picture } = ticket.getPayload();
-
-        const user = await googleUser(name, email, picture)
-
-        // Enviar al front
-        res.status(201).json(user)
-=======
         const token = req.body.token;
         async function verify() {
             const ticket = await client.verifyIdToken({
                 idToken: token,
-                audience: process.env.GOOGLE_APP_CLIENT_ID,  
+                audience: process.env.GOOGLE_APP_CLIENT_ID,
             });
             const payload = ticket.getPayload();
-            const userID = payload['sub'];
+            const userId = payload.sub
             console.log('payload: ', payload)
+            
+            const user = await googleUser(payload.given_name, payload.email, payload.picture)
 
+            req.session.userId = user.id
+
+            console.log(req.session)
+
+            console.log(user)
         }
->>>>>>> b1c95f6f7587fa7067fb014d6670b4319e6dfa3d
 
         verify().catch(console.error);
     } catch (e) {
@@ -91,19 +54,7 @@ const googleAuth = async (req, res) => {
         return
 
     }
-    }
-
-        // Enviar al front
-        // res.status(201).json(user)
-        // console.log(req.body)
-
-    // } catch (e) {
-
-    //     res.status(401).send(e.message)
-    //     return
-
-    // }
-
+}
 
 
 module.exports = googleAuth
