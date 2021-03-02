1c9be004b7713f140fc1425bf75b1d28b33537da
lorenabao <lorena.bao@outlook.com>
2021-03-02 20:57:49 +0100

    mas css

--- front/src/Content/QuestionPreview.js
+++ front/src/Content/QuestionPreview.js
@@ -41,12 +41,12 @@ function QuestionPreview({ question }) {
             <main className='question-preview'>
                 <h3>
                     <NavLink to={`/question/${question.id}`} className="question-preview-title"> {question.title} </NavLink>
-                    {question.languages && 
+                </h3>
+                {question.languages && 
                         question.languages.map(language => 
                             <Link to={'/questions?languages=' + language} className="question-preview-languages"> {language} </Link>
                         )
                     } 
-                </h3>
                 <div className='question-preview-excerpt'>
                     {question.body.substring(0, 250) + ' ...'}
                 </div>
--- front/src/assets/components/NotFound.js
+++ front/src/assets/components/NotFound.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './NotFound.css'
 
 function NotFound() {
     return (
-        <div>
-            <h3>Error 404. No encontramos lo que buscas.</h3>
+        <div className='not-found'>
+            <div className='imagen-404' />
+            <div className='message-404'>
+                <h3 className='error-message'>Error 404. No encontramos lo que buscas.</h3>
+                <Link to='/Home'>Volver</Link>
+            </div>
         </div>
     )
 }
